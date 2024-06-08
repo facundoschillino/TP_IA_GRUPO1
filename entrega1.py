ac8ba385d7e5dc7ad20bb79b1ab790db11602eda
federicoFrancesconi <ffedericofrancesconi@gmail.com>
2024-06-08 14:57:46 -0300

    Optimizacion actions

--- entrega1.py
+++ entrega1.py
@@ -1,11 +1,13 @@
 from simpleai.search import (
     SearchProblem,
-    greedy,
-    astar,)
+    greedy, astar,
+    )
+
 from simpleai.search.viewers import (BaseViewer, WebViewer)
+
 import itertools
 
-inicial_ejemplo = (
+INITIAL_STATE = (
         ("verde", "azul", "rojo", "naranja"),     # frasco 1, notar el orden de los colores
         ("azul", "rosa", "naranja"),              # frasco 2, notar que es de largo 3, queda un espacio vacío
         ("rosa", "celeste", "verde", "verde"),    # frasco 3, notar cómo "verde" se repite 2 veces por los 2 cuartos iguales
@@ -16,78 +18,99 @@ inicial_ejemplo = (
         ("lila", "lila", "lila"),                 # frasco 8, notar la repetición de colores para cada cuarto
         ())
 
+def esta_cerrado (frasco):
+    un_color = set(frasco)
+    return len(frasco) == 4 and len(un_color) == 1
 
-def cerrado (jarro): #Funcion para verificar que un jarro no este cerrado
-    un_color = set(jarro)
-    if (len(jarro) == 4 and len(un_color)==1):
-        return True
-    return False
+def esta_vacio (frasco):
+    return len(frasco) > 0
+
+def color_superior(frasco):
+    indice_color_superior = len(frasco) - 1
+    color_superior = frasco[indice_color_superior]
+    return color_superior
+
 class SortEmAll(SearchProblem):
-    def actions(self,state): #Vamos a tener que ver el color del ultimo item y determinar a que frasco lo vamos a trasvasar
+
+    def actions(self, state):
+        frascos = state
+        # Vamos a tener que ver el color del ultimo item y determinar a que frasco lo podemos a trasvasar
         actions = []
-        for origen,jarro_origen in enumerate(state):#recorro la lista de jarros, para ver desde que jarro voy a trasvasar
-            if len(jarro_origen) > 0 and not cerrado(jarro_origen): # verifico que no este vacío o cerrado
-                indice_ultimo_color_origen = len(jarro_origen)-1
-                ultimo_color_origen = jarro_origen[indice_ultimo_color_origen] #Guardo el ulitmo color para ver a cual puedo trasvasar
-                for destino,jarro_destino in enumerate(state): # #recorro la lista de jarros, para ver hacia que jarro voy a trasvasar
-                    if jarro_origen != jarro_destino and len(jarro_destino) > 0: #Primero, voy a probar con los frascos que tienen el mismo color encima, descartando los frascos vacíos, ya que a esos siempre puedo trasvasar
-                        indice_ultimo_color_destino = len(jarro_destino)-1
-                        ultimo_color_destino = jarro_destino[indice_ultimo_color_destino]
-                        if (ultimo_color_destino == ultimo_color_origen and len(jarro_destino)<4):
-                            actions.append((origen, destino))
-                    if len(jarro_destino) == 0:
-                         actions.append((origen, destino))
+        # Recorro la lista de frascos, para ver desde que frasco puedo trasvasar
+        for indice_origen, frasco_origen in enumerate(frascos):
+            if not esta_vacio(frasco_origen) and not esta_cerrado(frasco_origen):
+                # Recorro la lista de frascos para encontrar el destino
+                for indice_destino, frasco_destino in enumerate(frascos):
+                    # Ignoro el mismo frasco origen
+                    if indice_destino != indice_origen:
+                        # Ignoro los frascos llenos
+                        if len(frasco_destino) < 4:
+                            # Agrego los frascos vacios
+                            if len(frasco_destino) == 0:
+                                actions.append((indice_origen, indice_destino))
+                            # Agrego los frascos que tienen el mismo color superior
+                            elif color_superior(frasco_origen) == color_superior(frasco_destino): 
+                                actions.append((indice_origen, indice_destino))
+
         return actions
-    def result(self, state,action):
+    
+    def result(self, state, action):
         origen,destino = action
         state = list(state)
-        jarro_origen = list(state[origen])
-        jarro_destino = list(state[destino])
-        color_a_trasvasar = jarro_origen[len(jarro_origen)-1]
+        frasco_origen = list(state[origen])
+        frasco_destino = list(state[destino])
+        color_a_trasvasar = frasco_origen[len(frasco_origen)-1]
         cantidad_al_final = 0
-        for color in jarro_origen:
+        for color in frasco_origen:
             if color == color_a_trasvasar:
                 cantidad_al_final += 1
             else:
                 cantidad_al_final = 0
         #Hasta acá lo que hice fue ver cuantas unidades de ese color tengo al final, osea, cuantas puedo trasvasar.
-        if (len(jarro_destino) == 0): ##Este es para trasvasar a uno vacio
+        if (len(frasco_destino) == 0): ##Este es para trasvasar a uno vacio
              for _ in range(cantidad_al_final):
-                color_sustraido = jarro_origen.pop()
-                jarro_destino.append(color_sustraido)
+                color_sustraido = frasco_origen.pop()
+                frasco_destino.append(color_sustraido)
         else: #Este es para cuando destino no esta vacio y tengo que contar cuantos puedo trasvasar
-             faltante_destino = 4 - len(jarro_destino)
+             faltante_destino = 4 - len(frasco_destino)
              if (faltante_destino >= cantidad_al_final):
                 for _ in range(cantidad_al_final):
-                    color_sustraido = jarro_origen.pop()
-                    jarro_destino.append(color_sustraido)
+                    color_sustraido = frasco_origen.pop()
+                    frasco_destino.append(color_sustraido)
              else:
-                  while (len(jarro_destino) < 4):
-                    color_sustraido = jarro_origen.pop()
-                    jarro_destino.append(color_sustraido)                 
-        jarro_origen = tuple(jarro_origen)
-        jarro_destino = tuple(jarro_destino)
-        state[origen] = jarro_origen
-        state[destino] = jarro_destino
+                  while (len(frasco_destino) < 4):
+                    color_sustraido = frasco_origen.pop()
+                    frasco_destino.append(color_sustraido)                 
+        frasco_origen = tuple(frasco_origen)
+        frasco_destino = tuple(frasco_destino)
+        state[origen] = frasco_origen
+        state[destino] = frasco_destino
         return tuple(state)
+    
     def cost(self, state, actions, state1): #Por lo que entiendo, lo que hay que minimizar son los movimientos, asi que cada accion vale 1
         return 1  
+    
     def is_goal(self, state): # Esto no se si esta bien, es una idea
-        for jarro in state:
-            if len(jarro) > 0 and not cerrado(jarro):
+        for frasco in state:
+            if len(frasco) > 0 and not esta_cerrado(frasco):
                 return False
         return True
-    def heuristic(self,state): #Solo voy a contar los jarros que no estan cerrados
+    
+    def heuristic(self,state): #Solo voy a contar los frascos que no estan cerrados
         no_cerrados = 0
-        for jarro in state:
-            if len(jarro) >0 and not cerrado(jarro):
+        for frasco in state:
+            if len(frasco) >0 and not esta_cerrado(frasco):
                 no_cerrados +=1
         return no_cerrados
 
-my_problem = SortEmAll(inicial_ejemplo)
+my_problem = SortEmAll(INITIAL_STATE)
+
 #v = BaseViewer()
+
 v = WebViewer()
+
 result = astar(my_problem, viewer=v)
+
 if result is None:
     print("No solution")
 else:

d823e02b6eb852d860fe5460395c73eb44a08224
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-06-20 03:31:31 -0300

    .

--- entrega2.py
+++ entrega2.py
@@ -1,29 +1,41 @@
-from simpleai.search import (
-    CspProblem,
-    csp, 
-    SearchProblem, 
-    backtrack, 
-    min_conflicts
-)
-#Todos los frascos tienen capacidad de 4 segmentos. Esto lo satisfacemos definiendo las variables
-#Todos los frascos deben llenarse hasta el tope. El problema tiene que ser completo, asi que esto se satisface en la definición
-variables = {}  
-dominio= ["Celeste", "Blanco", "Rojo", "Verde"] 
-restricciones = []
-#Fede: esta variable la recibe de la funcion armar_nivel (una vez que ya este implementado el CSP y ande)
-CANTIDAD_COLORES = 4
-#Fede: Hay que generar los cuartos, no los frascos
-#Fede: Podria armarse una tupla con 1A, 1B, 1C, y asi
-def generar_variables(n): #Aca simplemente generamos la cantidad de frascos que necesitamos
+from simpleai.search import (CspProblem, backtrack, min_conflicts,
+                             MOST_CONSTRAINED_VARIABLE,
+                             LEAST_CONSTRAINING_VALUE)
+variables = [] 
+dominio = {}
+def generar_variables(n):  
     lista_tuplas = []
-    for x in range(1, n+1):
+    for x in range(1, n + 1):
         for y in range(1, 5):
             lista_tuplas.append((x, y))
     return lista_tuplas
-variables = generar_variables(CANTIDAD_COLORES)
-#Una vez que este generado el csp, hacemos una funcion que nos retorne los frascos armados
-#Restriccion que verifica que cada color solo aparezca 4 veces:
-def solo_4(vars, vals):
+def armar_nivel(colores, contenido_seleccionado):
+    variables = generar_variables(len(colores))
+    dominio = {var: colores for var in variables}
+    for indice_frasco, frasco in enumerate(contenido_seleccionado):
+        for indice, color in enumerate(frasco):
+            dominio[(indice_frasco + 1, indice + 1)] = color
+    cantidad_colores = len(colores)
+
+
+    restricciones.append((variables, solo_4))
+    restricciones.append((variables, no_resuelto))
+    restricciones.append((variables, todos_al_fondo))
+    restricciones.append((variables, compartir_color))
+    restricciones.append((variables, que_no_haya_6))
+    restricciones.append((variables, todos_distintos))
+    problema = CspProblem(variables, dominio, restricciones)
+    solucion = backtrack(problema, variable_heuristic=MOST_CONSTRAINED_VARIABLE, value_heuristic=LEAST_CONSTRAINING_VALUE)
+
+
+    frascos = [[] for _ in range(cantidad_colores)]
+    for posicion in solucion:
+        frasco, cuarto = posicion
+        frascos[frasco -1].append(solucion[posicion])
+    return frascos
+
+restricciones = []
+def solo_4(vars, vals): 
     colores = {}
     for valor in vals:
         if valor in colores:
@@ -34,9 +46,8 @@ def solo_4(vars, vals):
         if conteo > 4:
             return False
     return True
-restricciones.append((variables, solo_4))
-#Ningún frasco debe comenzar resuelto. Es decir, ningún frasco debe tener 4 segmentos del mismo color. Resuelto
 def no_resuelto(vars, vals):
+    CANTIDAD_COLORES = len(set(vals))  
     for frasco in range(1, CANTIDAD_COLORES + 1):
         colores_frasco = []
         for posicion_frasco in range(1, 5):
@@ -48,91 +59,48 @@ def no_resuelto(vars, vals):
         if len(set(colores_frasco)) == 1:
             return False
     return True
-restricciones.append((variables, no_resuelto))
-#Ningún color puede comenzar con todos sus segmentos en el fondo de frascos, porque se trataría de una situación excesivamente difícil de resolver. Restriccion
-def todos_al_fondo(vars,vals): 
-    colores_Fondo = []
-    for var, val in zip(vars, vals):
-        frasco, cuarto = var
-        if cuarto == 1: #Si es el primer cuarto del frasco (Osea digamos el que esta al fondo) lo agrego a la lista
-            color_del_fondo = val #Me guardo el color del fondo
-            colores_Fondo.append(val) # Lo aniado a la lista
-    conteo = {} #Diccionario que voy a usar para llevar la cantidad de apariciones
-    for color in colores_Fondo:
-        if color in conteo:
-            conteo[color] += 1
-        else:
-            conteo[color] = 1
-    for color in conteo:
-        if conteo[color] >= 4:
-            return False
-    return True
-restricciones.append((variables, todos_al_fondo))
-#Si dos frascos son adyacentes, deben compartir al menos un color. #Restriccion
+def todos_al_fondo(vars, vals):  
+    colores_fondo = [val for var, val in zip(vars, vals) if var[1] == 1]
+    conteo = {color: colores_fondo.count(color) for color in colores_fondo}
+    return all(c < 4 for c in conteo.values())
+
 def compartir_color(vars, vals):
-    frascos = [[] for _ in range(CANTIDAD_COLORES)]  # Inicializa la lista de frascos
-    # Llena los frascos con sus valores correspondientes
+    CANTIDAD_COLORES = len(set(vals))  
+    frascos = [[] for _ in range(CANTIDAD_COLORES)]
     for var, val in zip(vars, vals):
         frasco, cuarto = var
         frascos[frasco - 1].append(val)
-    # Verifica que frascos adyacentes compartan al menos un color
     for indice, frasco in enumerate(frascos):
-        interseccion1 = set()
-        interseccion2 = set()
-        frasco_anterior = []
-        frasco_posterior = []
-        if indice > 0:
-            frasco_anterior = set(frascos[indice - 1])
-            interseccion1 = set(frasco).intersection(frasco_anterior)
-        if indice < CANTIDAD_COLORES - 1:
-            frasco_posterior = set(frascos[indice + 1])
-            interseccion2 = set(frasco).intersection(frasco_posterior)
-        if not interseccion1 and not interseccion2:
+        if indice > 0 and not set(frasco).intersection(frascos[indice - 1]):
             return False
-    return True 
-restricciones.append((variables, compartir_color))
-#Si dos frascos son adyacentes, no pueden tener más de 6 colores diferentes entre ambos, para evitar situaciones demasiado complejas. #Restriccion
-
-def que_no_haya_6(vars, vals): 
-    frascos = [[] for _ in range(CANTIDAD_COLORES)]  # Inicializo la lista de frascos
-    #Genero los frascos con sus colores
-    for var, val in zip(vars, vals):
-        frasco, cuarto = var
-        frascos[frasco - 1].append(val)
-    for indice, frasco in enumerate(frascos):
-        frasco_anterior = []
-        frasco_posterior = []
-        if indice > 0:
-            frasco_anterior = frascos[indice - 1]  
-            frasco_anterior.extend(frasco)
-        if indice < CANTIDAD_COLORES -1:
-            frasco_posterior = frascos[indice + 1]       
-            frasco_posterior.extend(frasco)
-        if len(set(frasco_anterior)) > 6 or len(set(frasco_posterior)) > 6:
+        if indice < CANTIDAD_COLORES - 1 and not set(frasco).intersection(frascos[indice + 1]):
             return False
     return True
-restricciones.append((variables, que_no_haya_6))
-
-#No puede haber dos frascos exáctamente iguales. Restriccion.
-def todos_distintos(vars, vals):
-    frascos = [[] for _ in range(CANTIDAD_COLORES)]  # Inicializo la lista de frascos
-    #Genero los frascos con sus colores
+def que_no_haya_6(vars, vals):
+    CANTIDAD_COLORES = len(set(vals)) 
+    frascos = [[] for _ in range(1,CANTIDAD_COLORES+1)]
     for var, val in zip(vars, vals):
         frasco, cuarto = var
         frascos[frasco - 1].append(val)
-restricciones.append((variables, todos_distintos))        
+    for indice, frasco in enumerate(frascos):
+        if indice > 0 and len(set(frasco + frascos[indice - 1])) > 6:
+            return False
+        if indice < CANTIDAD_COLORES - 1 and len(set(frasco + frascos[indice + 1])) > 6:
+            return False
+    return True
+def todos_distintos(vars, vals):
+    CANTIDAD_COLORES = len(set(vals))   
+    frascos = [[] for _ in range(CANTIDAD_COLORES)]
+    for var, val in zip(vars, vals):
+        frasco, cuarto = var
+        frascos[frasco - 1].append(val)
+    return len(set(tuple(frasco) for frasco in frascos)) == CANTIDAD_COLORES
 
-problema = CspProblem(variables, {var: dominio for var in variables}, restricciones)
-solucion = min_conflicts(problema)
-print (solucion)
-    
-    
-    
-    
-    
-    
-    #Lo que se me ocurre es agregar colores que estan en el fondo del frasco
-    #Fede: es buena esa. Podes contar la cantidad de apariciones de cada color en el fondo. Si alguno tiene 4 apariciones, no se cumple la restriccion
+frascos = armar_nivel(colores=["rojo", "verde", "azul", "amarillo"],  # 4 colores, por lo que deberemos armar 4 frascos
+    contenido_seleccionado=[
+        ("verde", "azul", "rojo", "rojo"),    
+        ("verde", "rojo"),                     
+    ],
+)
 
-#Comienzo a agregar las restricciones
-#Para la restriccion no_resuelto tienen que agruparse las variables de un mismo frasco    
+print(frascos)
--- prueba.py
+++ prueba.py
@@ -1,89 +1,32 @@
-from simpleai.search import CspProblem, backtrack
+variables = [] 
+dominio = []
+def generar_variables(n):  
+    lista_tuplas = []
+    for x in range(1, n + 1):
+        for y in range(1, 5):
+            lista_tuplas.append((x, y))
+    return lista_tuplas
+colores = ['Celeste', 'Blanco', 'Rojo', 'Verde']
+dominio = {(1, 1): 'Celeste', (1, 2): 'Celeste', (1, 3): 'Celeste', (1, 4): 'Blanco', (2, 1): 'Celeste', (2, 2): 'Blanco', (2, 3): 'Blanco', (2, 4): 'Rojo', (3, 1): 'Blanco', (3, 2): 'Rojo', (3, 3): 'Rojo', (3, 4): 'Verde', (4, 1): 'Rojo', (4, 2): 'Verde', (4, 3): 'Verde', (4, 4): 'Verde'}
 
-# Cantidad de frascos/colores
-CANTIDAD_COLORES = 4
 
-# Generar variables para cada segmento de cada frasco
-variables = [(frasco, segmento) for frasco in range(1, CANTIDAD_COLORES + 1) for segmento in range(1, 5)]
 
-# Dominio de colores
-dominio = ["Celeste", "Blanco", "Rojo", "Verde"]
 
-# Inicializar restricciones
-restricciones = []
 
-# Restricción: No puede haber más de 4 segmentos de un mismo color en total
-def restriccion_solo_4(variables, valores):
-    conteo_colores = {color: 0 for color in dominio}
-    for valor in valores:
-        conteo_colores[valor] += 1
-    return all(conteo <= 4 for conteo in conteo_colores.values())
 
-restricciones.append((variables, restriccion_solo_4))
-
-# Restricción: Ningún frasco debe comenzar resuelto (no 4 segmentos del mismo color en un frasco)
-def restriccion_no_resuelto(frasco):
-    def restriccion(frasco_vars, frasco_vals):
-        return len(set(frasco_vals)) > 1
-    frasco_vars = [(frasco, segmento) for segmento in range(1, 5)]
-    restricciones.append((frasco_vars, restriccion))
-
-for frasco in range(1, CANTIDAD_COLORES + 1):
-    restriccion_no_resuelto(frasco)
-
-# Restricción: Ningún color puede comenzar con todos sus segmentos en el fondo de frascos
-def restriccion_todos_al_fondo(variables, valores):
-    fondo_colores = [valores[i] for i in range(0, len(valores), 4)]
-    conteo_colores = {color: fondo_colores.count(color) for color in dominio}
-    return all(conteo < 4 for conteo in conteo_colores.values())
-
-restricciones.append((variables, restriccion_todos_al_fondo))
-
-# Restricción: Si dos frascos son adyacentes, deben compartir al menos un color
-def restriccion_compartir_color(frasco1, frasco2):
-    def restriccion(vars, vals):
-        colores_frasco1 = set(vals[:4])
-        colores_frasco2 = set(vals[4:])
-        return len(colores_frasco1 & colores_frasco2) > 0
-    frasco1_vars = [(frasco1, segmento) for segmento in range(1, 5)]
-    frasco2_vars = [(frasco2, segmento) for segmento in range(1, 5)]
-    restricciones.append((frasco1_vars + frasco2_vars, restriccion))
-
-for frasco in range(1, CANTIDAD_COLORES):
-    restriccion_compartir_color(frasco, frasco + 1)
-
-# Restricción: Si dos frascos son adyacentes, no pueden tener más de 6 colores diferentes entre ambos
-def restriccion_no_seis_colores(variables, valores):
-    for i in range(CANTIDAD_COLORES - 1):
-        frasco1_valores = valores[i*4:(i+1)*4]
-        frasco2_valores = valores[(i+1)*4:(i+2)*4]
-        if len(set(frasco1_valores + frasco2_valores)) > 6:
+def compartir_color(vars, vals, CANTIDAD_COLORES):  
+    frascos = [[] for _ in range(CANTIDAD_COLORES)]
+    for var, val in zip(vars, vals):
+        frasco, cuarto = var
+        frascos[frasco - 1].append(val)
+    for indice, frasco in enumerate(frascos):
+        if indice > 0 and not set(frasco).intersection(frascos[indice - 1]):
+            return False
+        if indice < CANTIDAD_COLORES - 1 and not set(frasco).intersection(frascos[indice + 1]):
             return False
     return True
+keys = list(dominio.keys())
+values = list(dominio.values())
 
-restricciones.append((variables, restriccion_no_seis_colores))
-
-# Restricción: No puede haber dos frascos exactamente iguales
-def restriccion_frascos_distintos(frasco1, frasco2):
-    def restriccion(vars, vals):
-        return vals[:4] != vals[4:]
-    frasco1_vars = [(frasco1, segmento) for segmento in range(1, 5)]
-    frasco2_vars = [(frasco2, segmento) for segmento in range(1, 5)]
-    restricciones.append((frasco1_vars + frasco2_vars, restriccion))
-
-for frasco1 in range(1, CANTIDAD_COLORES + 1):
-    for frasco2 in range(frasco1 + 1, CANTIDAD_COLORES + 1):
-        restriccion_frascos_distintos(frasco1, frasco2)
-
-# Definir el problema CSP
-problema = CspProblem(variables, {var: dominio for var in variables}, restricciones)
-
-# Resolver el problema
-solucion = backtrack(problema)
-
-# Mostrar la solución
-print("Solución encontrada:")
-for frasco in range(1, CANTIDAD_COLORES + 1):
-    print(f"Frasco {frasco}:")
-    for segmento in range(1, 5):
-        print(f"  Segmento {segmento}: {solucion[(frasco, segmento)]}")
+resultado = compartir_color(keys, values, 4)
+print(resultado)
